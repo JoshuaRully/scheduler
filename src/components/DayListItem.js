a764b85be35207287755bb36e60a51e113a1472e
Joshua Rully <joshrully@gmail.com>
2021-03-15 08:31:32 -0700

    DayListItem basic functionality complete

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,23 @@
 import React from "react";
 
+const classNames = require("classnames");
+
+// helper function for future use
+// const spotsFormat = spots => {
+//   if (!spots) {
+//     return `no spots remaining`;
+//   }
+//   if (spots === 1) {
+//     return `$spots spot remaining`;
+//   }
+//   return `${spots} spots remaining`;
+// }
+
 export default function DayListItem(props) {
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--regular">X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots}</h3>
     </li>
   );
 }
