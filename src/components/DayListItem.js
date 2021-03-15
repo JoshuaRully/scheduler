fe0d57dca6c715f90d2d0f7f9407f4fde8747394
Joshua Rully <joshrully@gmail.com>
2021-03-15 09:26:19 -0700

    formatSpots function implented to pass test cases

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,18 +2,18 @@ import React from "react";
 import "components/DayListItem.scss";
 const classNames = require("classnames");
 
-const spotsFormat = spots => {
+const formatSpots = spots => {
   if (!spots) {
     return `no spots remaining`;
   }
   if (spots === 1) {
-    return `$spots spot remaining`;
+    return `${spots} spot remaining`;
   }
   return `${spots} spots remaining`;
 }
 
 export default function DayListItem(props) {
-  const availabilityMsg = spotsFormat(props.spots);
+  const availabilityMsg = formatSpots(props.spots);
 
   let dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
