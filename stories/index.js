c13a3f224e2474ddbb54cd1e2e279772902ecc27
Joshua Rully <joshrully@gmail.com>
2021-03-16 15:06:25 -0700

    add Empty comp. w/ event handler

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 // Button ~
 storiesOf("Button", module)
@@ -150,3 +151,4 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment w/ Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm"/>)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
