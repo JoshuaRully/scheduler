48c5c62a82a50f8ee92d6871cd20cdbcd6a1cb69
Joshua Rully <joshrully@gmail.com>
2021-03-16 17:32:33 -0700

    add basic form functionality

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,16 +4,30 @@ import Button from "components/Button";
 
 export default function Form(props) {
   // states below
+  const [currentName, setName] = useState(props.name || "");
   const [currentInterviewer, setInterviewer] = useState(props.value || null)
+
+  //Helper function to clear all fields
+  const reset = () => {
+    setName("")
+    setInterviewer(null)
+  }
+
+  function cancel () {
+    reset();
+    props.onCancel()
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name={props.name}
             type={props.message}
+            onChange={(event) => setName(event.target.value)}            
+            value={currentName}
             placeholder="Enter Student Name"
             /*
               This must be a controlled component
@@ -24,8 +38,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
-          <Button confirm>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onSubmit={event => event.preventDefault()} onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
