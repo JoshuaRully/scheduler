fd2bdb29ec51752e5c6cbc0caffd1eb5d3282562
Joshua Rully <joshrully@gmail.com>
2021-03-18 14:07:14 -0700

    implement custom hook ~ useVisualMode ~ passing all tests

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,13 +20,13 @@ export default function Application() {
     Promise.resolve(axios.get('http://localhost:8001/api/appointments')),
     Promise.resolve(axios.get('http://localhost:8001/api/interviewers'))
   ]).then((all) => {
-    setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewer: all[2].data}));
+    setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
   });
 
   const appointmentObjects = getAppointmentsForDay(state, state.day);
-  const interview = getInterview(state, appointmentObjects.interview)
 
   const appointment = appointmentObjects.map((appointmentObject) => {
+    const interview = getInterview(state, appointmentObjects.interview)
     return (
       <Appointment {...appointmentObject} key={appointmentObject.id} interview={interview}/>
     );
